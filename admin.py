7306de980cc2999d2ce4aa21314de5ab99f0c7e3
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2023-07-28 21:25:37 -0600

    Update admin.py

--- admin.py
+++ admin.py
@@ -8,15 +8,15 @@ data = pd.read_excel('database.xlsx',  sheet_name = 'Data', engine='openpyxl')
 
 st.dataframe(data)
 
-# Create a form to create a new record
-st.form("Create new record")
-name = st.text_input("Name")
-email = st.text_input("Email")
+# # Create a form to create a new record
+# st.form("Create new record")
+# name = st.text_input("Name")
+# email = st.text_input("Email")
 
-# If the form is submitted, add the new record to the Excel file
-if st.form_submitted():
-    data = data.append({"Name": name, "Email": email}, ignore_index=True)
-    data.to_excel(excel_file)
+# # If the form is submitted, add the new record to the Excel file
+# if st.form_submitted():
+#     data = data.append({"Name": name, "Email": email}, ignore_index=True)
+#     data.to_excel(excel_file)
 
-# Display the data in a dataframe
-st.dataframe(data)
+# # Display the data in a dataframe
+# st.dataframe(data)
