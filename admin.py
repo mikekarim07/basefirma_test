bce9210108b9a8e640eaecb0302bedfc96ea1c51
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2023-07-29 17:43:16 -0600

    Update admin.py

--- admin.py
+++ admin.py
@@ -18,7 +18,7 @@ st.header('KPIs Dashboard')
 data = pd.read_csv('database.csv')
 
 # Display the data in a dataframe
-st.dataframe(data)
+st.expander(st.dataframe(data))
 
 # Create a form to create a new record
 with st.form("Create new record"):
