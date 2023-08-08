3f6b02ad7443160875d8b363e44d8bab5479ded8
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2023-08-07 18:16:55 -0600

    Update admin.py

--- admin.py
+++ admin.py
@@ -68,11 +68,12 @@ kpis_empleado = data.query(
 )
 total_tareas = kpis_empleado['Cumplio'].count()
 tareas_cumplidas = len(kpis_empleado[kpis_empleado['Cumplio']=='Si'])
-tareas_no_cumplidas = total_tareas - tareas_cumplidas
+tareas_no_cumplidas = tareas_cumplidas-total_tareas
 pct_cumplimiento = (tareas_cumplidas/total_tareas)
 pct_cumplimiento = "{:.2%}".format(pct_cumplimiento)
 
-st.metric(label="Tareas Cumplidas", value= tareas_cumplidas) #, delta=tareas_no_cumplidas)#
+# Assuming you have calculated tareas_cumplidas and tareas_no_cumplidas
+st.metric(label="Tareas Cumplidas", value=tareas_cumplidas, delta=tareas_no_cumplidas)
 st.metric(label="Total Tareas", value= total_tareas)
 st.metric(label="% Cumplimiento", value= pct_cumplimiento)
 st.metric(label="Tareas No Cumplidas", value= tareas_no_cumplidas)
