a5defc5cc8fe6a36b7526d57d248df8ec7c4f14b
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2023-08-07 18:09:03 -0600

    Update admin.py

--- admin.py
+++ admin.py
@@ -68,10 +68,10 @@ kpis_empleado = data.query(
 )
 total_tareas = kpis_empleado['Cumplio'].count()
 tareas_cumplidas = len(kpis_empleado[kpis_empleado['Cumplio']=='Si'])
-%cumplimiento = tareas_cumplidas/total_tareas
+pct_cumplimiento = tareas_cumplidas/total_tareas
 st.metric(label="Tareas Cumplidas", value= tareas_cumplidas) #, delta="1.2 °F"
-st.metric(label="Total Tareas", value= total_tareas) #, delta="1.2 °F"
-st.metric(label="% Cumplimiento", value= %cumplimiento) #, delta="1.2 °F"
+st.metric(label="Total Tareas", value= total_tareas)
+st.metric(label="% Cumplimiento", value= pct_cumplimiento)
 
 resumen_kpis_emp = kpis_empleado.groupby(by=['Consultor'], as_index=False).agg({'Tiempo estimado de Actividad': 'sum','Tiempo Real': 'sum'})
 
